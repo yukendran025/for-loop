8979e0b6b5b3e076cf3a11cfb2ac8a9eb06f9e13
yukendran025 <yukendransaravanan025@gmail.com>
2025-07-09 18:23:59 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in);
+       // int num = sc.nextInt();
+        for(int i=23;i<=57;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            }
+        }
+}
